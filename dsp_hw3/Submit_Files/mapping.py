960a2d26eb41f8a7c606e288d670f34d13d4fdd6
TonyHsu <sunprince12014@gmail.com>
2015-12-18 15:55:16 +0800

    助教說沒有iconv歐...OTL - 不寫trigram了 :p

--- dsp_hw3/Submit_Files/mapping.py
+++ dsp_hw3/Submit_Files/mapping.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import locale
 import codecs
 
 from_file = open(sys.argv[1])
@@ -15,20 +16,16 @@ from_map = from_file.read().splitlines()
 for line in from_map:
     word,tmp_spell = line.split(' ')
     spells = tmp_spell.split('/')
-    to_map[word] = word  #directly mapping
-    for spell in spells: #bopomofo mapping
-        #spell[:3] is the first
-        if spell[:3] in to_map:
-            to_map[spell[:3]].extend(word)
+    locale.setlocale(locale.LC_ALL,'zh_TW.BIG5')
+    to_file.write('{} {}\n'.format(word,word))
+    for spell in spells:
+        if(spell[0:2]) in to_map:
+            to_map[spell[0:2]].append(word)
         else:
-            to_map[spell[:3]] = []
-            to_map[spell[:3]].extend(word)
-
+            to_map[spell[0:2]] = []
+            to_map[spell[0:2]].append(word)
 for key in to_map:
-    to_file.write('{}'.format(key))
-    for idx in range(0,len(to_map[key]),3):
-        to_file.write(' {}'.format(''.join(to_map[key][idx:idx+3])))
-    to_file.write('\n')
+    to_file.write('{} {}\n'.format(key,' '.join(w for w in to_map[key])))
 
 from_file.close()
 to_file.close()
